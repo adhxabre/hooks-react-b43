f7eec4cebcf3d23518f8a0f035060479e320a611
adhxabre <abeldustin06@gmail.com>
2023-01-16 13:26:17 +0700

    inline styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
 
+import Form from "./components/form";
+
 function App() {
-  const [data, setData] = useState(0);
-
-  // let data = 0;
-
-  function Inc() {
-    return setData(data + 1);
-  }
-
-  function Dec() {
-    if (data === 0) return;
-    return setData(data - 1);
-  }
-
   return (
     <>
-      <h1>
-        {data} | {data !== 0 ? "true" : "false"}
-      </h1>
-      <button onClick={Inc}>Increment</button>
-      <button onClick={Dec}>Decrement</button>
+      <Form />
     </>
   );
 }
