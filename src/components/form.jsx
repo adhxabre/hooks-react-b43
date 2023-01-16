5535b19236be38c724fc39f4c3aeebef9c3d785a
adhxabre <abeldustin06@gmail.com>
2023-01-16 13:38:42 +0700

    css modules

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 
-const styles = {
-  form: {
-    margin: "16px 20% 0",
-  },
-  formGroup: {
-    marginBottom: "16px",
-  },
-  formLabel: {
-    marginBottom: "8px",
-    display: "inline-block",
-  },
-  formInput: {
-    display: "block",
-    width: "100%",
-    padding: ".375rem .75rem",
-    fontSize: "1rem",
-    lineHeight: 1.5,
-    color: "#212529",
-    backgroundColor: "#fff",
-    border: "1px solid orangered",
-    borderRadius: ".25rem",
-  },
-};
+import cssModules from "./form.module.css";
+
+// const styles = {
+//   form: {
+//     margin: "16px 20% 0",
+//   },
+//   formGroup: {
+//     marginBottom: "16px",
+//   },
+//   formLabel: {
+//     marginBottom: "8px",
+//     display: "inline-block",
+//   },
+//   formInput: {
+//     display: "block",
+//     width: "100%",
+//     padding: ".375rem .75rem",
+//     fontSize: "1rem",
+//     lineHeight: 1.5,
+//     color: "#212529",
+//     backgroundColor: "#fff",
+//     border: "1px solid orangered",
+//     borderRadius: ".25rem",
+//   },
+// };
 
 export default function Form() {
   return (
-    <form style={styles.form}>
-      <div style={styles.formGroup}>
-        <label htmlFor="username" style={styles.formLabel}>
+    <form className={cssModules.form}>
+      <div className={cssModules.formGroup}>
+        <label htmlFor="username" className={cssModules.formLabel}>
           Username
         </label>
         <input
           id="username"
           placeholder="Input username"
-          style={styles.formInput}
+          className={cssModules.formInput}
         />
       </div>
     </form>
