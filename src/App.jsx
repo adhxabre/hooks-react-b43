0cfbb9c79dbcf8b7b08ba1ebb4a7124d1e8f79da
adhxabre <abeldustin06@gmail.com>
2023-01-16 14:52:11 +0700

    useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React from "react";
 
-import Form from "./components/form";
+import { Routes, Route, Link } from "react-router-dom";
 
-import "./styles/global.css";
-
-import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
     <>
-      <Form />
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign In</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
     </>
   );
 }
