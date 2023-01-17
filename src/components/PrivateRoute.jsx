5d3da404f23431f2af5a06996a365a5c0a5bc914
adhxabre <abeldustin06@gmail.com>
2023-01-17 09:19:57 +0700

    Props Types

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 export default function PrivateRoute() {
-  const isSignin = false;
+  const isSignin = true;
 
   return isSignin ? <Outlet /> : <Navigate to="/signin" />;
 }
